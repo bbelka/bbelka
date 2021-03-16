387018ca2472612b64f9d2b83a20e67126df5b4f
bbtwots <brettbelka@gmail.com>
2021-03-16 11:09:40 -0700

    no credentials for create user

--- src/utils/API.js
+++ src/utils/API.js
@@ -20,15 +20,15 @@ export default {
         return axios.post(BASE_URL + "/api/url", url)
     },
     login: (userData) => {
-        return axios.post(BASE_URL + "/auth/login", userData, { withCredentials: true })
+        return axios.post(BASE_URL + "/auth/login", userData)
     },
     logout: () => {
-        return axios.get(BASE_URL + "/auth/logout", { withCredentials: true })
+        return axios.get(BASE_URL + "/auth/logout")
     },
     readSessions: function () {
-        return axios.get(BASE_URL + "/readsessions", { withCredentials: true })
+        return axios.get(BASE_URL + "/readsessions")
     },
     createUser: (userData) => {
-        return axios.post(BASE_URL + "/api/user", userData, { withCredentials: true })
+        return axios.post(BASE_URL + "/api/user", userData)
     }
 }
